0f6f5390a72db28bd7290afe96df7e5c5a185d0a
ChakraMang <chakrapanim1020@gmail.com>
2022-03-20 23:16:50 +0530

    Final Project Submission

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,10 +13,13 @@ router.get("/test-me", function (req, res) {
 })
 
 
-
+// creating author
 router.post("/BASE_URL/authors", AuthorController.createAuthor)
+// login the author and creating jwt token
 router.post("/login", BlogController.loginUser)
+// creating the blog
 router.post("/blogs", Mw.authentication,BlogController.createBlog)
+// getting all the blogs
 router.get("/blogs", Mw.authentication, BlogController.getBlogs)
 router.put("/blogs/:blogId", Mw.authentication, Mw.authorisation, BlogController.updateBlog)
 router.delete("/blogs/:blogId", Mw.authentication, Mw.authorisation,BlogController.deleteBlog)
